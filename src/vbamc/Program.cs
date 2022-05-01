80b3a7e9cc7fe55584adf8f5ca6106d4356df0e5
Jozef Izso <jozef.izso@gmail.com>
2022-05-01 22:10:25 +0200

    Generate presentation with macros from a template

--- src/vbamc/Program.cs
+++ src/vbamc/Program.cs
@@ -1,9 +1,11 @@
-﻿using System.Text;
+﻿using DocumentFormat.OpenXml.Packaging;
+using System.Text;
 using vbamc;
 
 Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
 
 var targetPath = @"vbaProject.bin";
+var targetMacroPath = @"CustomMacro.pptm";
 
 var compiler = new VbaCompiler();
 
@@ -14,3 +16,17 @@ compiler.AddModule(@"d:\dev\github\NetOfficeFw\vbamc\sample\Module1.vb");
 compiler.AddClass(@"d:\dev\github\NetOfficeFw\vbamc\sample\Class1.vb");
 
 compiler.Compile(targetPath);
+
+var macroTemplatePath = Path.Combine(Directory.GetCurrentDirectory(), @"data\MacroTemplate.potm");
+var macroTemplate = PresentationDocument.CreateFromTemplate(macroTemplatePath);
+var mainDoc = macroTemplate.PresentationPart;
+if (mainDoc != null)
+{
+    var vbaProject = mainDoc.AddNewPart<VbaProjectPart>();
+    using var reader = File.OpenRead(targetPath);
+    vbaProject.FeedData(reader);
+    reader.Close();
+}
+
+macroTemplate.ChangeDocumentType(DocumentFormat.OpenXml.PresentationDocumentType.MacroEnabledPresentation);
+macroTemplate.SaveAs(targetMacroPath);
