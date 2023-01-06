93e9eb9a24889da4b812805f9ee8199622d094ed
Jozef Izso <jozef.izso@gmail.com>
2023-01-06 17:40:26 +0100

    Update sample add-in

--- sample/Module1.vb
+++ sample/Module1.vb
@@ -3,5 +3,9 @@ Declare PtrSafe Function GetCurrentProcessId Lib "kernel32" () As Long
 
 Public Sub Auto_Open()
     ' execute code when application starts
+    ' MsgBox "Hello world from " & Application.Name & ", PID " & CStr(GetCurrentProcessId)
+End Sub
+
+Public Sub OnActionAbout(control As IRibbonControl)
     MsgBox "Hello world from " & Application.Name & ", PID " & CStr(GetCurrentProcessId)
 End Sub
