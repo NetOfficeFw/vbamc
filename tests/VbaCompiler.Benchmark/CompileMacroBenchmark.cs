003f34853f35d5d377b1478b776078fceb5caee9
Jozef Izso <jozef.izso@gmail.com>
2025-10-26 20:21:03 +0100

    Fix typo in Arrange/Act sections

--- tests/VbaCompiler.Benchmark/CompileMacroBenchmark.cs
+++ tests/VbaCompiler.Benchmark/CompileMacroBenchmark.cs
@@ -16,7 +16,7 @@ public class CompileMacroBenchmark
     [Benchmark]
     public MemoryStream CompileVbaProject()
     {
-        // Arrang
+        // Arrange
         var sourcePath = Path.Combine(Directory.GetCurrentDirectory(), "data");
         var classPath = Path.Combine(sourcePath, "Class.vb");
         var modulePath = Path.Combine(sourcePath, "Module.vb");
