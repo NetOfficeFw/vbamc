7009de2873e2d32e0847d475cf215ac41c1da94b
Jozef Izso <jozef.izso@gmail.com>
2025-11-22 17:08:59 +0100

    Update scripts to use .NET 10 runtime

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-dotnet publish -c Release -r osx.10.15-x64 -f net6.0 --self-contained true -o dist
+dotnet publish -c Release -r osx-arm64 -f net10.0 --self-contained true
